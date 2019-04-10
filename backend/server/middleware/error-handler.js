06487d6c10cecbd375ee293d7329e9f6c3e8fbf9
Vitali Timolyanov <nf.ebox@gmail.com>
2019-04-10 18:24:04 +0300

    Add error-handler

---
+++ backend/server/middleware/error-handler.js
@@ -0,0 +1,38 @@
+const { RESPONSE_JSON_ERROR_NAME } = require('../../config');
+
+function setErrorJSON(obj = {}, errorMessage = '') {
+  return { ...obj, [RESPONSE_JSON_ERROR_NAME]: errorMessage };
+}
+
+function setErrorResponse(err, ctx) {
+  ctx.status = err.status || 500;
+
+  const defaultMessage = ctx.response.message;
+  const errorMessage = err.expose
+    ? `${defaultMessage}: ${err.message}`
+    : defaultMessage;
+
+  if (ctx.accepts('json')) {
+    ctx.body = setErrorJSON({}, errorMessage);
+  } else {
+    ctx.body = errorMessage;
+  }
+}
+
+async function handler(ctx, next) {
+  try {
+    await next();
+
+    if (ctx.status === 404) {
+      setErrorResponse({ status: 404, message: ctx.response.message }, ctx);
+    }
+  } catch (err) {
+    setErrorResponse(err, ctx);
+
+    ctx.app.emit('error', err, ctx);
+  }
+}
+
+module.exports = function errorHandler() {
+  return handler;
+};
