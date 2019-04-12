4dd13d60221d9b734dcb8ebd8697debfe03f8a9b
Vitali Timolyanov <nf.ebox@gmail.com>
2019-04-12 05:30:07 +0300

    Add create/update features to /user endpoint

--- backend/src/server/routes/v1/user.js
+++ backend/src/server/routes/v1/user.js
@@ -1,4 +1,5 @@
 const Router = require('koa-router');
+const koaBody = require('koa-body');
 const services = require('../../../services');
 
 const { ENDPOINT_PREFIX_USER } = require('../../../config');
@@ -6,6 +7,11 @@ const { ENDPOINT_PREFIX_USER } = require('../../../config');
 const router = new Router({
   prefix: ENDPOINT_PREFIX_USER,
 });
+
+function getUserProfile(ctx) {
+  // TODO: parse body
+  return ctx.request.body;
+}
 
 async function getUser(ctx) {
   const userId = ctx.params.id;
@@ -18,6 +24,37 @@ async function getUser(ctx) {
   ctx.assert(ctx.body, 404, 'User not found');
 }
 
+async function createUser(ctx) {
+  // TODO: validate payload
+
+  try {
+    ctx.body = await services.createUser(getUserProfile(ctx));
+  } catch (err) {
+    if (err.code === '23505') {
+      ctx.throw(409, 'Email already used by another account', { error: err });
+    }
+    ctx.throw(500, 'Cannot create user', { error: err });
+  }
+}
+
+async function updateUser(ctx) {
+  // TODO: validate payload
+
+  try {
+    ctx.body = await services.updateUser({
+      ...getUserProfile(ctx),
+      id: ctx.params.id,
+    });
+  } catch (err) {
+    if (err.code === '23505') {
+      ctx.throw(409, 'Email already used by another account', { error: err });
+    }
+    ctx.throw(500, 'Cannot update user', { error: err });
+  }
+}
+
 router.get('/:id', getUser);
+router.post('/', koaBody(), createUser);
+router.post('/:id', koaBody(), updateUser);
 
 module.exports = router;
