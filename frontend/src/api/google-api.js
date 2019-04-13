af72b2756b490978d0dd4cee6b9d489d54185e08
AlexandrChebotar <44335524+AlexandrChebotar@users.noreply.github.com>
2019-04-13 17:32:21 +0300

    Add class Place instead of gapi PlaceResult interface

--- frontend/src/api/google-api.js
+++ frontend/src/api/google-api.js
@@ -1,15 +1,38 @@
 import {MAP_SEARCH_PLACE_TYPES, MAP_SEARCH_PLACE_NAMES, MAP_CHECK_PLACE_TYPES} from '../constants'
 
+class Place {
+  constructor(placeResult) {
+    const {place_id, geometry, name, vicinity, types, rating, user_ratings_total} = placeResult;
+    this.placeId = place_id;
+    this.location = geometry.location;
+    this.name = name;
+    this.adress = vicinity;
+    this.tags = types;
+    this.type = this.getType();
+    this.rating = {
+      value: rating,
+      users: user_ratings_total,
+    };
+  }
+  getType() {
+    for (const type of MAP_CHECK_PLACE_TYPES) {
+      if (this.tags.includes(type)) {
+        return type;
+      }
+    }
+  }
+}
+
 function _searchNearbyPlaces(placesService,  searchRequest) {
   return new Promise((resolve, reject) => {
-      let places = [];
+      let placeResults = [];
       placesService.nearbySearch(searchRequest, (results, status, pagination) => {
           if (status === 'OK' || status === 'ZERO_RESULTS') {
-            places = places.concat(results);
+            placeResults = placeResults.concat(results);
             if (pagination.hasNextPage) {
               pagination.nextPage();
             } else {
-              resolve(places);
+              resolve(placeResults);
             }
           } else {
             reject(status);
@@ -18,7 +41,7 @@ function _searchNearbyPlaces(placesService,  searchRequest) {
   })
 }
 
-export function searchPlaces(placesService, location, radius) {
+export function searchMedicPlaces(placesService, location, radius) {
   const searchRequests = [
     ...MAP_SEARCH_PLACE_TYPES.map(type => {
       return _searchNearbyPlaces(placesService, {location, radius, type});
@@ -30,18 +53,21 @@ export function searchPlaces(placesService, location, radius) {
   return Promise.all(searchRequests)
     .then((results) => {
       const exceededMaxPlacesNumber = results.some((result) => result.length === 60);
-      let places = results.reduce((places, result) => places.concat(result));
+      let placeResults = results.reduce((placeResults, result) => placeResults.concat(result));
       let unicPlaceIds = [];
-      places = places.filter((place) => {
-        if (!place.types.some((type) => MAP_CHECK_PLACE_TYPES.includes(type))) {
+      placeResults = placeResults.filter((placeResult) => {
+        if (!placeResult.types.some((type) => MAP_CHECK_PLACE_TYPES.includes(type))) {
           return false;
         }
-        if (unicPlaceIds.includes(place.place_id)) {
+        if (unicPlaceIds.includes(placeResult.place_id)) {
           return false;
         }
-        unicPlaceIds.push(place.place_id);
+        unicPlaceIds.push(placeResult.place_id);
         return true;
       });
-    return {places, exceededMaxPlacesNumber};
+      const places = placeResults.map(placeResult => {
+        return new Place(placeResult);
+      });
+      return {places, exceededMaxPlacesNumber};
   });
 }
