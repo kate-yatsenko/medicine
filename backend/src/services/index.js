e6dc0987a7038d1a12ed3377f4896ccfe05bc541
Vitali Timolyanov <nf.ebox@gmail.com>
2019-04-14 05:13:29 +0300

    Update services with entry create/update methods

--- backend/src/services/index.js
+++ backend/src/services/index.js
@@ -1,43 +1,62 @@
 const db = require('../db');
 
-module.exports = {
-  // async createEntry({
-  //   ownerId,
-  //   creatorId,
-  //   entryTypeId,
-  //   title,
-  //   description,
-  //   result,
-  // }) {
-  //   const entryId = await db.entry.createEntry({
-  //     ownerId,
-  //     creatorId,
-  //     entryTypeId,
-  //     title,
-  //     description,
-  //     result,
-  //   });
-  //   return this.getEntry(entryId);
-  // },
+function formEntry(entry) {
+  const e = entry;
+  return {
+    id: e.id,
+    owner: { id: e.ownerId, name: e.ownerName },
+    creator: { id: e.creatorId, name: e.creatorName },
+    type: {
+      id: e.entryTypeId,
+      name: e.entryTypeName,
+      description: e.entryTypeDescription,
+    },
+    title: e.title,
+    description: e.description,
+    result: e.result,
+    created: e.created,
+  };
+}
 
-  async getOwnerEntries({ ownerId }) {
-    const entries = await db.entry.getOwnerEntries({ ownerId });
-    return entries.map(e => {
-      return {
-        id: e.id,
-        owner: { id: e.ownerId, name: e.ownerName },
-        creator: { id: e.creatorId, name: e.creatorName },
-        type: {
-          id: e.entryTypeId,
-          name: e.entryTypeName,
-          description: e.entryTypeDescription,
-        },
-        title: e.title,
-        description: e.description,
-        result: e.result,
-        created: e.created,
-      };
+module.exports = {
+  async createEntry({
+    ownerId,
+    creatorId,
+    typeId,
+    title,
+    description,
+    result,
+  }) {
+    const entryId = await db.entry.createEntry({
+      ownerId,
+      creatorId,
+      typeId,
+      title,
+      description,
+      result,
     });
+    return this.getEntry(entryId);
+  },
+
+  async getEntry(id) {
+    const entry = await db.entry.getEntry(id);
+    return formEntry(entry);
+  },
+
+  async getEntries({ ownerId = null, creatorId = null, typeId = null }) {
+    const entries = await db.entry.getEntries({ ownerId, creatorId, typeId });
+    return entries.map(formEntry);
+  },
+
+  async updateEntry({ id, title, description, result }) {
+    const entryId = await db.entry.updateEntry({
+      id,
+      title,
+      description,
+      result,
+    });
+
+    return entryId ? this.getEntry(entryId) : entryId;
   },
 
   async createUser({ name, email, gender, phone, address, birth }) {
@@ -66,6 +85,7 @@ module.exports = {
       address,
       birth,
     });
-    return this.getUser(userId);
+
+    return userId ? this.getUser(userId) : userId;
   },
 };
