ea629416166b9377a70adca69d96f2e46ff01c85
Vitali Timolyanov <nf.ebox@gmail.com>
2019-04-14 05:14:54 +0300

    Add POSTs (create/update) to /entry endpoint

--- backend/src/server/routes/v1/entry.js
+++ backend/src/server/routes/v1/entry.js
@@ -9,48 +9,53 @@ const router = new Router({
   prefix: ENDPOINT_PREFIX_ENTRY,
 });
 
-// function getPayload(ctx) {
-//   // TODO: parse body
-//   return ctx.request.body;
-// }
+function getPayload(ctx) {
+  // TODO: parse body
+  return ctx.request.body;
+}
 
 async function getEntries(ctx) {
   // TODO: validate id
+  // TODO: switch owner ot creator if user is a doctor
   const { uid: ownerId } = ctx.params;
 
   try {
-    ctx.body = await services.getOwnerEntries({ ownerId });
+    ctx.body = await services.getEntries({ creatorId: ownerId });
   } catch (err) {
     ctx.throw(500, 'Cannot get entries', { error: err });
   }
   ctx.assert(ctx.body, 404, 'Entry not found');
 }
 
-// async function createEntry(ctx) {
-//   // TODO: validate payload
+async function createEntry(ctx) {
+  // TODO: validate payload
 
-//   try {
-//     ctx.body = await services.createEntry(getPayload(ctx));
-//   } catch (err) {
-//     ctx.throw(500, 'Cannot create entry', { error: err });
-//   }
-// }
+  try {
+    ctx.body = await services.createEntry(getPayload(ctx));
+  } catch (err) {
+    ctx.throw(500, 'Cannot create entry', { error: err });
+  }
+}
 
-// async function updateEntry(ctx) {
-//   // TODO: validate payload
+async function updateEntry(ctx) {
+  // TODO: validate payload
 
-//   try {
-//     ctx.body = await services.updateUser({
-//       ...getPayload(ctx),
-//       id: ctx.params.id,
-//     });
-//   } catch (err) {
-//     ctx.throw(500, 'Cannot update entry', { error: err });
-//   }
-// }
+  try {
+    ctx.body = await services.updateEntry({
+      ...getPayload(ctx),
+      id: ctx.params.id,
+    });
+  } catch (err) {
+    ctx.throw(500, 'Cannot update entry', { error: err });
+  }
 
+  ctx.assert(ctx.body, 404, 'Entry not found');
+}
+
+// TODO: id validator middleware
 router.get('/', getEntries);
-// router.post('/', koaBody(), createEntry);
-// router.post('/:id', koaBody(), updateEntry);
+// router.get('/id', getEntries);
+router.post('/', koaBody(), createEntry);
+router.post('/:id', koaBody(), updateEntry);
 
 module.exports = router;
