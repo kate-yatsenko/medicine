9fad37efaf05f74294904adf4a4dca6d4a41ee22
Vitali Timolyanov <nf.ebox@gmail.com>
2019-04-15 02:53:03 +0300

    Update /db/user with getList (user list) method

--- backend/src/db/user.js
+++ backend/src/db/user.js
@@ -1,5 +1,7 @@
 const knex = require('./knex');
 
+const { SEARCH_RESULTS_LIMIT } = require('../config');
+
 module.exports = {
   createUser({ name, email, gender, phone, address, birth }) {
     return knex('user')
@@ -54,5 +56,14 @@ module.exports = {
       .where({ id: userId })
       .update({ roleId }, ['id'])
       .then(ids => ids[0]);
+  },
+
+  getList({ name, excludeId }) {
+    return knex({ u: 'user' })
+      .where('u.name', 'ilike', `%${name}%`)
+      .andWhereNot({ 'u.id': excludeId })
+      .limit(SEARCH_RESULTS_LIMIT)
+      .orderBy('u.name')
+      .select('u.id', 'u.name', 'u.email', 'u.birth');
   },
 };
