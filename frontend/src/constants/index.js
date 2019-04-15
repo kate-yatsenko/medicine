3f0900fe2a470c56ae848c721d469e4b52a7351e
AlexandrChebotar <44335524+AlexandrChebotar@users.noreply.github.com>
2019-04-15 03:00:54 +0300

    Refactor constants usage

--- frontend/src/constants/index.js
+++
@@ -1,3 +0,0 @@
-export * from './map';
-
-export const TOGGLE_AUTH_MODAL_VISIBLE = 'TOGGLE_AUTH_MODAL_VISIBLE';
---
+++ frontend/src/constants/mapConstants.js
@@ -0,0 +1,28 @@
+export const INIT_MAP_SERVICES = 'INIT_MAP_SERVICES';
+export const SEARCH_PLACES = 'SEARCH_PLACES';
+export const SET_PLACES_FILTER = 'SET_PLACES_FILTER';
+export const SEARCH_PLACE_TYPES = [
+  'hospital',
+  'doctor',
+  'physiotherapist',
+  'dentist',
+  'pharmacy',
+];
+export const HOSPITAL_NAMES= [
+  'больница',
+  'лікарня',
+]
+export const DOCTOR_NAMES = [
+  'клиника',
+  'клініка',
+  'врач',
+  'лікар',
+];
+export const SEARCH_PLACE_NAMES = [
+  ...HOSPITAL_NAMES,
+  ...DOCTOR_NAMES,
+];
+export const CHECK_PLACE_TYPES = [
+  ...SEARCH_PLACE_TYPES,
+  'health',
+];
