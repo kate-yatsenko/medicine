0b95eff972a0ed2fb0dc81893013dcd8b94d824a
AlexandrChebotar <44335524+AlexandrChebotar@users.noreply.github.com>
2019-04-19 22:31:58 +0300

    Add toggle place selection

--- frontend/src/pages/HomePage/Map/MapDashboard/PlacesList/index.js
+++ frontend/src/pages/HomePage/Map/MapDashboard/PlacesList/index.js
@@ -3,25 +3,30 @@ import {List} from 'antd';
 
 export default class PlacesList extends Component {
 
+  toggleSelectPlace = (placeId) => {
+    const zoom = this.props.map.getZoom();
+    const prevActivePlaceId = this.props.places.activePlaceId;
+    const activePlaceId = prevActivePlaceId !== placeId ? placeId : prevActivePlaceId ? null : placeId;
+    this.props.selectPlace({activePlaceId, zoom});
+  }
   getPlacesList = ({placeId, name, adress, type, tags, rating, ratingUsers}) => {
     return (
       <List.Item 
         key={placeId} 
         style={{cursor:'pointer',}}
-        onClick={() => {
-          const zoom = this.props.map.getZoom();
-          this.props.selectPlace({activePlaceId: placeId, zoom});
-        }}
+        onClick={() => this.toggleSelectPlace(placeId)}
       >
-        {name}
-        {placeId === this.props.places.activePlaceId &&
-          <div style={{paddingLeft: '10px', fontStyle: 'italic', width: '250px'}}>
-            {adress}<br />
-            {type}<br />
-            {tags.join(', ')}<br />
-            {`рейтинг: ${rating?rating:'-'}/5 (відгуків - ${ratingUsers?ratingUsers:0})`}
-          </div>
-        }
+        <div className="map-places-list-item">
+          {name}
+          {placeId === this.props.places.activePlaceId &&
+            <div style={{paddingLeft: '10px', fontStyle: 'italic', width: '250px'}}>
+              {adress}<br />
+              {type}<br />
+              {tags.join(', ')}<br />
+              {`рейтинг: ${rating?rating:'-'}/5 (відгуків - ${ratingUsers?ratingUsers:0})`}
+            </div>
+          }
+        </div>
       </List.Item>
     )
   }
