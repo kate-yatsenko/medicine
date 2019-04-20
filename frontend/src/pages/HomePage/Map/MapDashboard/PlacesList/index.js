2e7689beae258da2cc09f7bf90d34bc3a1fe3cce
AlexandrChebotar <44335524+AlexandrChebotar@users.noreply.github.com>
2019-04-21 01:05:14 +0300

    Add places filter function

--- frontend/src/pages/HomePage/Map/MapDashboard/PlacesList/index.js
+++ frontend/src/pages/HomePage/Map/MapDashboard/PlacesList/index.js
@@ -6,21 +6,29 @@ import './style.css';
 const Search = Input.Search;
 
 export default class PlacesList extends Component {
+  state = {
+    filter: null,
+  }
 
+  setFilter = (filter) => this.setState({filter});
   toggleSelectPlace = (placeId) => {
     const zoom = this.props.map.getZoom();
     const prevActivePlaceId = this.props.places.activePlaceId;
     const activePlaceId = prevActivePlaceId !== placeId ? placeId : (prevActivePlaceId ? null : placeId);
     this.props.selectPlace({activePlaceId, zoom});
-  }
-  getTagsList(tags) {
-    return tags.map((tag) => {
-      if (!(tag === 'point_of_interest' || tag === 'establishment')){
-        return <span key={tag} className="map-tag" >{tag}</span>;
-      }
-      return '';
-    });
-  }
+  };
+  getFilteredPlaces = () => {
+    const filter = this.state.filter;
+    const placesArray = this.props.places.placesArray;
+    if (filter) {
+      return placesArray.filter(({name}) => {
+        const nameLC = name.toLowerCase();
+        const filterLC = filter.toLowerCase();
+        return nameLC.includes(filterLC);
+      });
+    }
+    return placesArray;
+  };
   getPlacesList = ({placeId, name, adress, tags, rating, ratingUsers}) => {
     const {toggleSelectPlace, getTagsList} = this;
     const active = placeId === this.props.places.activePlaceId;
@@ -42,9 +50,18 @@ export default class PlacesList extends Component {
         </div>
       </List.Item>
     )
+  };
+  getTagsList(tags) {
+    return tags.map((tag) => {
+      if (!(tag === 'point_of_interest' || tag === 'establishment')){
+        return <span key={tag} className="map-tag" >{tag}</span>;
+      }
+      return '';
+    });
   }
 
   render() {
+    const {getFilteredPlaces, getPlacesList} = this;
     return (
       <div className="map-places-list">
         <Search
@@ -57,15 +74,15 @@ export default class PlacesList extends Component {
               <Icon type="filter" />
             </Button>
           }
-          // TODO onSearch={}
-          // style={{ width: 200 }}
+          onChange={(e) => this.setFilter(e.target.value)}
+          onSearch={(filter) => this.setFilter(filter)}
         />
         <div style={{overflow: 'auto', height:'400px'}}>
           <List
               size="small"
               bordered={false}
-              dataSource={this.props.places.placesArray}
-              renderItem={this.getPlacesList}
+              dataSource={getFilteredPlaces()}
+              renderItem={getPlacesList}
           />
         </div>
       </div>
