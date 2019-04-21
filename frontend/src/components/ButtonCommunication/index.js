7978fc43fe92e9f9061e14bd32bab637322bf87c
Kate <yatsenko.km@gmail.com>
2019-04-21 18:31:32 +0300

    chat-page-api: fix message button

--- frontend/src/components/ButtonCommunication/index.js
+++ frontend/src/components/ButtonCommunication/index.js
@@ -18,7 +18,7 @@ const ButtonCommunication = ({ totalMessages, token, currentCompanion }) => {
   return token ? (
     <Link to="/chat">
       <div className="fixed-button">
-        <Badge count={currentCompanion ? Number(totalMessages) - currentCompanion.unread : Number(totalMessages)} overflowCount={99}>
+        <Badge count={Number(totalMessages)} overflowCount={99}>
           <Tooltip placement="top" title="Відкрити чат">
             <Button shape="circle" icon="mail"/>
           </Tooltip>
